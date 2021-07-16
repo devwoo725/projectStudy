8709027d98677850676676658fa6858b703379c9
devwoo <devwoo725@gmail.com>
2021-07-16 16:11:02 +0900

    Spring boot project setting

---
+++ src/main/java/com/devwoo/projectstudy/ProjectStudyApplication.java
@@ -0,0 +1,13 @@
+package com.devwoo.projectstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectStudyApplication.class, args);
+    }
+
+}
